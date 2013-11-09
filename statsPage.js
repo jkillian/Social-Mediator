dc53aa8f31d3151099e972a882679fc1d4bbe06a
JKillian <jasonkillian35@gmail.com>
2013-11-09 16:11:15 -0500

    top charts

--- statsPage.js
+++ statsPage.js
@@ -1,5 +1,7 @@
 $(function() {
 
+   resizeCanvas('overviewChart');
+
    var NUM_TOP_SITES = 3;
    var COLORS = ["#ff0000", "#00ff00", "#0000ff"];
 
@@ -10,25 +12,46 @@ $(function() {
       "actualWork.com": 1000*60*2
    };
 
+   siteData = getSiteDataFromStorage();
+
    var dataArr = [];
    for(var domain in siteData) {
       dataArr.push({site: domain, secondsOnSite: siteData[domain]/1000});
    }
-
    dataArr.sort(function(site1, site2) {
       return site1.secondsOnSite - site2.secondsOnSite;
    });
 
    var chartData = [];
-
    for(var i=0; i<dataArr.length && i<NUM_TOP_SITES; ++i) {
       var site = dataArr[i];
       chartData.push({value: site.secondsOnSite, color: COLORS[i % COLORS.length] });
+      console.log("Site: " + site.site + " Time: " + site.secondsOnSite + " Color: " + COLORS[i % COLORS.length]);
    }
 
-   console.log("context: " + " " + ctx);
    var ctx = document.getElementById("overviewChart").getContext("2d");
    var myNewChart = new Chart(ctx).Doughnut(chartData, {});
-   console.log("Chart!" + " " + myNewChart);
+
+
+
+   function getSiteDataFromStorage() {
+      var returnData = {};
+      var numSites = localStorage.length;
+      for(var i=0; i<numSites; ++i)
+      {
+         var key = localStorage.key(i);
+         returnData[key] = localStorage.getItem(key);
+      }
+      return returnData;
+   }
+
+
+
+   function resizeCanvas(canvasId) {
+      var cDiv = $('#' + canvasId + 'Div');
+      var canvas = document.getElementById(canvasId);
+      canvas.height = cDiv.innerHeight();
+      canvas.width = cDiv.innerWidth();
+   }
 });
 
